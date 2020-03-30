3928564603a6e3133465c0555ab9c941c5e68450
Steven Ly <1stevenly92@gmail.com>
2020-03-30 10:35:22 -0700

    attempted to do practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
 jQuery(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
+
+
+var tags = ["h1" , "p" , "img"]
+
+tags.forEach(function(tag){
+    tags[tag].addEventListener("click", function () {
+      alert("This is a " + tag);
+
+    })
+
+// jQuery("h1").click(function(){
+//   alert("this is" + tags[0] )
+// })
+
 });
 
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-});
 
-  jQuery("img").click(function() {
-    alert("This is an image.");
-});
+  
+
+
+
+//   jQuery("h1").click(function() {
+//     alert("This is a header.");
+//     alert("I told you, THIS IS A HEADER!");
+// });
+
+//   jQuery("p").click(function() {
+//     alert("This is a paragraph.");
+// });
+
+//   jQuery("img").click(function() {
+//     alert("This is an image.");
+// });
 });
